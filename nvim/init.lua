a26e975eb659ad9b56807777f872187bc3b2d1a4
Gustavo Machado <gluismachado@outlook.com>
2024-06-05 13:39:10 -0300

    Remover tmux e nvim, movidos para outro repositório

--- nvim/init.lua
+++
@@ -1,4 +0,0 @@
--- Requires
-require('gustavo.core.global_settings') -- Load global settings
-require('gustavo.core.keyboard_shortcuts') -- Load keyboard shortcuts
-require('gustavo.core.plugin_manager') -- Load plugin manager
--- nvim/lua/gustavo/plugins/split_maximizer.lua
+++
@@ -1,7 +0,0 @@
--- Vim maximizer
-return {
-    'szw/vim-maximizer', -- Clone the split window maximizer
-    keys = {
-        { '<leader>sm', '<cmd>MaximizerToggle<CR>' }, -- Load split window maximizer
-    },
-}
