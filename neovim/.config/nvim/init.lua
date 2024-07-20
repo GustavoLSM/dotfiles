06f8f07187fe415fdbf222fcbe541f7cb1a5b97a
Gustavo Machado <gluismachado@outlook.com>
2024-07-20 13:21:44 -0300

    Adicionar git ignore e remover VIM

--- neovim/.config/nvim/init.lua
+++
@@ -1,4 +0,0 @@
--- Requires
-require('gustavo.core.global_setting') -- Load global settings
-require('gustavo.core.keyboard_shortcuts') -- Load keyboard shortcuts
-require('gustavo.core.plugin_manager') -- Load plugin manager
