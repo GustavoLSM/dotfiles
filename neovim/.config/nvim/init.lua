25e6288669356f6093728a2c05ab9d6656decee7
Gustavo Machado <gluismachado@outlook.com>
2024-07-20 13:06:16 -0300

    Teste restaura nvim

--- neovim/.config/nvim/init.lua
+++ neovim/.config/nvim/init.lua
@@ -1,4 +1,4 @@
 -- Requires
-require('gustavo.core.global_settings') -- Load global settings
+require('gustavo.core.global_setting') -- Load global settings
 require('gustavo.core.keyboard_shortcuts') -- Load keyboard shortcuts
 require('gustavo.core.plugin_manager') -- Load plugin manager
