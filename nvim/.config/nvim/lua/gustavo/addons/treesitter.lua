b6630f90c0d2fa02b1dc7dbab6cd9c5e0b6af97c
Gustavo Machado <gluismachado@outlook.com>
2024-07-30 16:38:20 -0300

    Adicionar syntax highlight

---
+++ nvim/.config/nvim/lua/gustavo/addons/treesitter.lua
@@ -0,0 +1,10 @@
+return {
+	"nvim-treesitter/nvim-treesitter", 
+	build = ":TSUpdate",
+	config = function()
+		require("nvim-treesitter.configs").setup({
+			ensure_installed = { "php", "html", "javascript", "lua" },
+			highlight = { enable = true },
+		})
+	end
+}
