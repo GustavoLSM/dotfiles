03cc0b16cccf66501b6f8e11196a29241032b988
Gustavo Machado <gluismachado@outlook.com>
2024-07-30 16:29:14 -0300

    Configurações do editor

---
+++ nvim/.config/nvim/lua/gustavo/config.lua
@@ -0,0 +1,15 @@
+vim.cmd("set termguicolors")
+vim.cmd("set background=dark")
+
+vim.cmd("set number")
+vim.cmd("set relativenumber")
+vim.cmd("set nowrap")
+
+vim.cmd("let g:netrw_liststyle=3")
+
+vim.cmd("set tabstop=4")
+vim.cmd("set shiftwidth=4")
+vim.cmd("set softtabstop=4")
+
+vim.cmd("set ignorecase")
+vim.cmd("set smartcase")
