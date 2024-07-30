156a710df9c40b80406501e9095e253b20122c4f
Gustavo Machado <gluismachado@outlook.com>
2024-07-30 16:29:35 -0300

    Atalhos de teclado

---
+++ nvim/.config/nvim/lua/gustavo/remap.lua
@@ -0,0 +1,8 @@
+vim.g.mapleader = " "
+
+vim.keymap.set("n", "<leader>e", ":Ex<CR>")
+
+vim.keymap.set("n", "<leader>nh", ":nohl<CR>")
+
+vim.keymap.set("n", "<leader>bb", ":bprevious<CR>")
+vim.keymap.set("n", "<leader>bn", ":bnext<CR>")
