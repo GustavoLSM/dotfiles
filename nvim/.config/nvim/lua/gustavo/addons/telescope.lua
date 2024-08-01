396ca09f2cd70b074c4f578190ebef9f588b1cca
Gustavo Machado <gluismachado@outlook.com>
2024-08-01 15:07:07 -0300

    Ajustar atalhos do Telescope

--- nvim/.config/nvim/lua/gustavo/addons/telescope.lua
+++ nvim/.config/nvim/lua/gustavo/addons/telescope.lua
@@ -8,7 +8,6 @@ return {
 		local builtin = require("telescope.builtin")
 
 		vim.keymap.set("n", "<C-f>", builtin.find_files, {})
-		vim.keymap.set("n", "<C-h>", builtin.git_files, {})
 		vim.keymap.set("n", "<leader>f", builtin.live_grep, {})
 
 		file_ignore_patterns = { "node_modules" }
