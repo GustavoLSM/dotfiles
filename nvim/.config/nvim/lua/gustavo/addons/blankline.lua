b5802da3b880f01cd760598be9b5bcdb4927765c
Gustavo Machado <gluismachado@outlook.com>
2024-08-01 15:40:58 -0300

    Remover espaços em branco dos arquivos

--- nvim/.config/nvim/lua/gustavo/addons/blankline.lua
+++ nvim/.config/nvim/lua/gustavo/addons/blankline.lua
@@ -1,6 +1,6 @@
-return { 
-	"lukas-reineke/indent-blankline.nvim", 
-	main = "ibl", 
+return {
+	"lukas-reineke/indent-blankline.nvim",
+	main = "ibl",
 	config = function()
 		require("ibl").setup()
 	end
