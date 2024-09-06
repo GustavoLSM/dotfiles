e71216d5be87fe6e893cc943f1c6beea687a2e19
Gustavo Machado <gluismachado@outlook.com>
2024-09-06 18:03:23 -0300

    Habilitar auto completar de código

---
+++ neovim/.config/nvim/lua/external/plugins/code_completion.lua
@@ -0,0 +1,27 @@
+return {
+	"hrsh7th/nvim-cmp",
+	dependencies = {
+		"hrsh7th/cmp-buffer",
+		"hrsh7th/cmp-path",
+	},
+	config = function()
+		local cmp = require("cmp")
+
+		cmp.setup({
+			snippet = {
+				expand = function(args)
+					require("luasnip").lsp_expand(args.body)
+				end
+			},
+			mapping = cmp.mapping.preset.insert({
+				["<C-Space>"] = cmp.mapping.complete(),
+				["<C-e>"] = cmp.mapping.abort(),
+				["<CR>"] = cmp.mapping.confirm({ select = true })
+			}),
+			sources = cmp.config.sources({
+				{ name = "buffer" },
+				{ name = "path" },
+			})
+		})
+	end
+}
