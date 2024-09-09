70de00f074e692eeb4ee71cab4053f434e4f5ec2
Gustavo Machado <gluismachado@outlook.com>
2024-09-09 15:21:37 -0300

    Adicionar atalho para copiar seleção para o clipboard do sistema

--- neovim/.config/nvim/lua/core/keyboard_shortcuts.lua
+++ neovim/.config/nvim/lua/core/keyboard_shortcuts.lua
@@ -6,6 +6,8 @@ vim.keymap.set("n", "<leader>nh", ":nohl<CR>")
 
 vim.keymap.set("n", "<leader>c", "* :%s//")
 
+vim.keymap.set("v", "Y", '"+y')
+
 vim.keymap.set("n", "<C-h>", "<C-w>h")
 vim.keymap.set("n", "<C-j>", "<C-w>j")
 vim.keymap.set("n", "<C-k>", "<C-w>k")
