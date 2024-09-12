9b6dce76807eb155a8874cb9ab0c831dfee23486
Gustavo Machado <gluismachado@outlook.com>
2024-09-12 15:58:26 -0300

    Adicionar regra para ignorar node_modules no fuzzy finder

--- neovim/.config/nvim/lua/external/plugins/fuzzy_finder.lua
+++ neovim/.config/nvim/lua/external/plugins/fuzzy_finder.lua
@@ -3,8 +3,24 @@ return {
 	tag = "0.1.8",
 	dependencies = { "nvim-lua/plenary.nvim" },
 	config = function()
-		local builtin = require('telescope.builtin')
-		vim.keymap.set('n', '<C-f>', builtin.find_files, {})
-		vim.keymap.set('n', '<leader>f', builtin.live_grep, {})
+		local builtin = require("telescope.builtin")
+
+		local file_ignore_patterns = { 
+			"node_modules/",
+			"package%-lock%.json"
+		}
+
+		vim.keymap.set("n", "<C-f>", function() 
+			builtin.find_files({ 
+				file_ignore_patterns = file_ignore_patterns
+			})
+		end)
+
+		vim.keymap.set("n", "<leader>f", function() 
+			builtin.live_grep({
+				file_ignore_patterns = file_ignore_patterns
+			})
+		end)
+
 	end
 }
