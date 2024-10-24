c37e762528dc2d09a39d30afcc42b7760b1c1be1
Gustavo Machado <gluismachado@outlook.com>
2024-10-24 09:44:05 -0300

    Adicionar configurações globais do neovim

---
+++ neovim/.config/nvim/lua/core/global_settings.lua
@@ -0,0 +1,10 @@
+vim.opt.guicursor = ""
+
+vim.cmd("set number")
+vim.cmd("set relativenumber")
+vim.cmd("set nowrap")
+
+vim.cmd("set noexpandtab")
+vim.cmd("set tabstop=4")
+vim.cmd("set softtabstop=4")
+vim.cmd("set shiftwidth=4")
